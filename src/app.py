114e5b43f35d1979f5b5bedd28106f847fac59cf
Thang Le <levietthang0512@outlook.com>
2022-04-21 06:48:12 +0700

    Initial Commit

---
+++ src/app.py
@@ -0,0 +1,29 @@
+# Create simple Discord bot that responds to messages with a random quote
+import random
+
+import discord
+from discord.ext import commands
+
+from credentials import TOKEN
+
+client = commands.Bot(command_prefix='!')
+
+@client.command("awake")
+async def awake(ctx):
+    await ctx.send("OOh, Just a little bit of sleep, I'm awake now!")
+
+@client.command("torengu")
+async def torengu(ctx):
+    await ctx.send("I'm not Ngu, Grrrr")
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+if __name__ == '__main__':
+    client.run(TOKEN)
+
+
