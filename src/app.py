2e2cbda521bdbd5a8f1371373f0c293cdf1f57c6
Thang Le <levietthang0512@outlook.com>
2022-04-22 08:39:28 +0700

    added translator

--- src/app.py
+++ src/app.py
@@ -5,26 +5,27 @@ import os
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix="!")
+from .translate.translate_api import GoogleTranslateAPI
 
+client = commands.Bot(command_prefix='!')
+translator = GoogleTranslateAPI()
 
 @client.command("awake")
 async def awake(ctx):
     await ctx.send("OOh, Just a little bit of sleep, I'm awake now!")
 
-
-@client.command("torengu")
-async def torengu(ctx):
-    await ctx.send("I'm not Ngu, Grrrr")
-
+@client.command("translate")
+async def translate(ctx, *, text):
+    await ctx.send(translator.translate(text))
 
 @client.event
 async def on_ready():
-    print("Logged in as")
+    print('Logged in as')
     print(client.user.name)
     print(client.user.id)
-    print("------")
+    print('------')
+
+if __name__ == '__main__':
+    client.run(os.environ['TOKEN'])
 
 
-if __name__ == "__main__":
-    client.run(os.environ["TOKEN"])
